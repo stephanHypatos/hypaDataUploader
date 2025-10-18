622739cb8eb3065327745fa8f973ee9a98a3ee77
Stephan <113194463+stephanHypatos@users.noreply.github.com>
2025-10-18 22:05:20 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -19,6 +19,7 @@ import streamlit as st
 DEFAULT_BASE_URL = "https://api.cloud.hypatos.ai"
 AUTH_PATH = "/v2/auth/token"
 ENRICHMENT_INSERT_PATH = "/v2/enrichment/invoices"
+SUPPLIER_INSERT_PATH = "/v2/enrichment/suppliers"
 
 # =========================
 # --- Auth
